ac5199684380da78dba4ff0ef3713ae0335c7397
Shobi <shobihap@stud.ntnu.no>
2018-04-13 11:10:28 +0200

    updated cost

--- OrderManager/OrderManager.go
+++ OrderManager/OrderManager.go
@@ -15,18 +15,28 @@ import (
       "../Fsm"
 )
 
-// key = string (ID/IP)
-// value = Elevator
-// get
+const (
+
+DOOR_OPEN_TIME int = 3000
+TRAVEL_TIME int = 2500
+includeCab bool = false
+
+)
+
+const ClearRequestType {
+    all = iota
+    inDirn = iota,
+}
+
+ClearRequestType clearRequestType = ClearRequestType.inDirn;
+
 var elevators = make(map[string]config.Elevator)
 
 var ExecuteOrders[config.N_FLOORS][config.N_BUTTONS] bool  //TEST!!
 
 var elev = make(chan config.Elevator)
 
-func OrderManager(ButtonPacketTrans chan config.ButtonPressPacket, ButtonPacketRecv chan config.ButtonPressPacket,
-     assignedOrders chan elevio.ButtonEvent, doorTimeout chan bool, ElevatorTrans chan config.Elevator,
-     ElevatorRecv chan config.Elevator, ButtonPress chan elevio.ButtonEvent, myID string, hwPort string)  {
+func OrderManager(ButtonPacketTrans chan config.ButtonPressPacket, ButtonPacketRecv chan config.ButtonPressPacket, assignedOrders chan elevio.ButtonEvent, doorTimeout chan bool, ElevatorTrans chan config.Elevator, ElevatorRecv chan config.Elevator, ButtonPress chan elevio.ButtonEvent, myID string, hwPort string)  {
 
     peerUpdateCh := make(chan peers.PeerUpdate)
     peerTxEnable := make(chan bool)
@@ -58,16 +68,14 @@ func OrderManager(ButtonPacketTrans chan config.ButtonPressPacket, ButtonPacketR
     		case buttonPress := <-ButtonPress:
     			fmt.Println("Button press at " + myID)
     			fmt.Println(buttonPress)
-                if buttonPress.Button == elevio.BT_Cab  {   //Differentiating between cab and hall orderss
+                if buttonPress.Button == elevio.BT_CAB  {   //Differentiating between cab and hall orderss
                     assignedOrders <- buttonPress
                     fmt.Println("CabOrder added")
-                    
+
                 }else{
                     ButtonPacketTrans <- config.ButtonPressPacket{myID, buttonPress.Floor, buttonPress.Button}
 
                 }
-
-    			
 
     		case recvPacket := <-ButtonPacketRecv:
                 //assignedOrders <- elevio.ButtonEvent{recvPacket.Floor, recvPacket.Button}
@@ -75,36 +83,8 @@ func OrderManager(ButtonPacketTrans chan config.ButtonPressPacket, ButtonPacketR
         		fmt.Println("Received from " + recvPacket.Sender)
         		fmt.Println(recvPacket.Floor, " ", recvPacket.Button)
 
-    	}
 
-    }
-
-    /*for{
-            select{
-            case p := <-peerUpdateCh:
-                fmt.Printf("Peer update:\n")
-                fmt.Printf("  Peers:    %q\n", p.Peers)
-                fmt.Printf("  New:      %q\n", p.New)
-                fmt.Printf("  Lost:     %q\n", p.Lost)
-
-            case buttonPress := <-ButtonPress:
-                fmt.Println("Button press at " + myID)
-                fmt.Println(buttonPress)		myID = fmt.Sprintf("peer-%s", localIP)
-
-                //floor <- Ch_floor
-                //OrderManager.executeOrder(buttonPress)
-                //fmt.Println("Change Made: ", changeMade)
-
-                ButtonPacketTrans <- config.ButtonPressPacket{myID, buttonPress.Floor, buttonPress.Button}
-                //ElevatorTrans <- Ch_elevator
-
-            case recvPacket := <-ButtonPacketRecv:
-                assignedOrders <- elevio.ButtonEvent{recvPacket.Floor, recvPacket.Button}
-
-                fmt.Println("Received from " + recvPacket.Sender)
-                fmt.Println(recvPacket.Floor, " ", recvPacket.Button)
-
-            	orderAccepted, changeMade := OrderManager.AddOrder(buttonPress)
+                /*orderAccepted, changeMade := OrderManager.AddOrder(buttonPress)
                 fmt.Println("Change Made: ", changeMade)
                 if changeMade {
                     ButtonPacketRecv <- ButtonPressPacket{myID, buttonPress.Floor, int(buttonPress.Button)}
@@ -116,11 +96,10 @@ func OrderManager(ButtonPacketTrans chan config.ButtonPressPacket, ButtonPacketR
                     if k != recvPacket.ID {
                         elevators[recvPacket.ID] = recvPacket
                     }
-                }
+                }*/
+    	}
 
-            }
-
-    }*/
+    }
 
 }
 
@@ -136,34 +115,71 @@ func addElevator(ip string, elevator config.Elevator)  {
 /*func executeOrder(buttonPress elevio.ButtonEvent, ) bool {
 
     cost := testCost()
-
     if (cost == true){
         AddOrder(buttonPress)
         return true
     }else{
       return false
     }
-}
+}*/
 
 //maa sammenligne kostene og legge til ordren dersom kosten returnerer true
-func AddOrder(buttonPress elevio.ButtonEvent) bool {
+/*func AddOrder(buttonPress elevio.ButtonEvent) bool {
 
     if (ExecuteOrders[buttonPress.Floor][buttonPress.Button] == false){
         ExecuteOrders[buttonPress.Floor][buttonPress.Button] = true
         return true
     }
-
     return false
+}*/
+
+/*func testCost() bool {
+  return true
+}*/
+
+/*func saveToDisk(filname string) error{
+
+	data, err := jason.Marshal( []byte, error )
+		if err != nil{
+			log.Println("Eroor: Failed to backup")
+			return err
+		}
+
+        //func WriteFile(filename string, data []byte, perm os.FileMode) error
+		if err := ioutil.WriteFile(filename, data , 0644); err != nil { // writes to file and checks for returned error
+			log.Println("Error: Failed to backup")
+			return err
+		}
+		return nil
+}*/
+
+/*func loadFromDisk(filename string) error { //func Stat(name string) (FileInfo, error)
+
+    var queue
+
+    if _, err := os.State(filename); err == nil {
+        data, err := ioutil.ReadFile(filename)
+        if err != nil{
+        log.Println("Error: Failed to read from backup")
+            return err
+    }
+
+    if err := jason.Unmarshal(data,queue); err != nil {
+        log.Println("Error: failed to Unmarshal")
+    }
+
+    }
+
+    return nil
+
 }*/
 
 //-----------------------------------------------------------------------------------------------------
 
-/*
-func testCost() bool {
-  return true
-}
-//the cost for a single elevator
-func cost(buttonPress elevio.ButtonEvent, e Elevator) int {
+// COST
+
+//the cost for a single elevator (MORTENFYHN)
+/*func cost(buttonPress elevio.ButtonEvent, e Elevator) int {
 
     cost := 0
     floor := e.floor
@@ -195,39 +211,8 @@ func cost(buttonPress elevio.ButtonEvent, e Elevator) int {
     }
 
     return cost
-}
-
-
-
-func requests_chooseDirection(e Elevator) MotorDirection {
-    switch e.dir {
-    case elevio.MD_Up:
-        if IsOrderAbove(e.floor) {
-            return elevio.MD_Up
-        } else if IsOrderBelow(e.floor) {
-            return elevio.MD_Down
-        }
-        return elevio.MD_Stop
-    case elevio.MD_Down:
-        if IsOrderBelow(e.floor) {
-            return elevio.MD_Down
-        } else if IsOrderAbove(e.floor) {
-            return elevio.MD_Up
-        }
-        return elevio.MD_Stop
-    case elevio.MD_Stop:
-        if IsOrderAbove(e.floor) {
-            return elevio.MD_Up
-        } else if IsOrderBelow(e.floor){
-            return elevio.MD_Down:
-        }
-        return elevio.MD_Stop
-    configault:
-        return elevio.MD_Stop
-    }
-}
-
-func incrementFloor(floor int, dir int) (int, int) {
+} */
+/*func incrementFloor(floor int, dir int) (int, int) {
 
     switch dir {
     case elevio.MD_Down:
@@ -246,91 +231,74 @@ func incrementFloor(floor int, dir int) (int, int) {
         floor = NumFloors - 1
     }
     return floor, dir
-}
+}*/
 
-/*
-func OrderManager()  {
-
-}
-
-
-func saveToDisk(filname string) error{
-
-	data, err := jason.Marshal( []byte, error )
-		if err != nil{
-			log.Println("Eroor: Failed to backup")
-			return err
-		}
-
-        //func WriteFile(filename string, data []byte, perm os.FileMode) error
-		if err := ioutil.WriteFile(filename, data , 0644); err != nil { // writes to file and checks for returned error
-			log.Println("Error: Failed to backup")
-			return err
-		}
-		return nil
-}
-
-func loadFromDisk(filename string) error { //func Stat(name string) (FileInfo, error)
-
-    var queue
-
-    if _, err := os.State(filename); err == nil {
-        data, err := ioutil.ReadFile(filename)
-        if err != nil{
-        log.Println("Error: Failed to read from backup")
-            return err
+func requests_chooseDirection(elev chan config.Elevator) MotorDirection {
+    switch elev.Direction {
+    case elevio.MD_Up:
+        if Fsm.IsOrderAbove(elev.Floor) {
+            return elevio.MD_Up
+        } else if Fsm.IsOrderBelow(elev.Floor) {
+            return elevio.MD_Down
+        }
+        return elevio.MD_Stop
+    case elevio.MD_Down:
+        if Fsm.IsOrderBelow(elev.Floor) {
+            return elevio.MD_Down
+        } else if Fsm.IsOrderAbove(elev.Floor) {
+            return elevio.MD_Up
+        }
+        return elevio.MD_Stop
+    case elevio.MD_Stop:
+        if Fsm.IsOrderAbove(elev.Floor) {
+            return elevio.MD_Up
+        } else if Fsm.IsOrderBelow(elev.Floor){
+            return elevio.MD_Down:
+        }
+        return elevio.MD_Stop
+    configault:
+        return elevio.MD_Stop
     }
-
-    if err := jason.Unmarshal(data,queue); err != nil {
-        log.Println("Error: failed to Unmarshal")
-    }
-
-    }
-
-    return nil
-
 }
 
-//-------------------------------------------------------------------------------------
-
-func requests_shouldStop(e Elevator) bool {
-    switch e.dir {
-    case MD_Down:
-        return e.requests[e.floor][BT_HallUp] || e.requests[e.floor][BT_Cab] || !IsOrderBelow(e.floor)
-    case MD_Up:
-        return e.requests[e.floor][BT_HallUp] || e.requests[e.floor][BT_Cab] || !IsOrderAbove(e.floor)
-    case MD_Stop:
+func requests_shouldStop(elev chan config.Elevator) bool {
+    switch elev.Direction {
+    case elevio.MD_Down:
+        return elev.AssignedRequests[elev.Floor][elevio.BT_HallDown] || elev.AssignedRequests[elev.Floor][elevio.BT_CAB] || !Fsm.IsOrderBelow(elev.Floor)
+    case elevio.MD_Up:
+        return elev.AssignedRequests[elev.Floor][elevio.BT_HallUp] || elev.AssignedRequests[elev.Floor][elevio.BT_CAB] || !Fsm.IsOrderAbove(elev.Floor)
+    case elevio.MD_Stop:
     configault:
         return true
     }
 }
 
-func request_clearAtCurrentFloor(e Elevator) Elevator {
-    switch e.config.onClearedRequestVariant {
+/*func request_clearAtCurrentFloor(elev chan config.Elevator) Elevator {
+    switch elev.config.onClearedRequestVariant {
     case CV_ALL:
         for btn Button := 0; btn < N_BUTTONS; btn++  {
-            e.requests[e.floor][btn] = 0
+            e.AssignedRequests[e.Floor][btn] = 0
         }
         break
     case CV_InDirn:
-        e.requests[e.floor][B_Cab] = 0
+        e.AssignedRequests[e.Floor][BT_CAB] = 0
         switch e.dirn {
         case D_Up:
-            e.requests[e.floor][B_HallUp] = 0
-            if !requests_above(e) {
-                e.requests[e.floor][B_HallDown] = 0
+            e.AssignedRequests[e.Floor][B_HallUp] = 0
+            if !Fsm.IsOrderAbove(e) {
+                e.AssignedRequests[e.Floor][B_HallDown] = 0
             }
             break
         case D_Down:
-            e.requests[e.floor][B_HallDown] = 0
-            if (!requests_below(e)) {
-                e.requests[e.floor][B_HallUp] = 0
+            e.AssignedRequests[e.Floor][B_HallDown] = 0
+            if (!Fsm.IsOrderBelow(e)) {
+                e.AssignedRequests[e.Floor][B_HallUp] = 0
             }
             break
         case D_Stop:
         configault:
-            e.requests[e.floor][B_HallUp] = 0
-            e.requests[e.floor][B_HallDown] = 0
+            e.AssignedRequests[e.Floor][B_HallUp] = 0
+            e.AssignedRequests[e.Floor][B_HallDown] = 0
             break
         }
         break
@@ -338,13 +306,14 @@ func request_clearAtCurrentFloor(e Elevator) Elevator {
         break
     }
     return e
-}
+}*/
 
+//NEED TO BE FIXED, void delegate(CallType c) onClearedRequest = null
 func request_clearAtCurrentFloor(e_old Elevator, onClearedRequest(b Button, floor int)) Elevator {
     e Elevator := e_old
     for btn Button := 0; btn < N_BUTTONS; btn++ {
-        if (e.requests[e.floor][btn]){
-            e.requests[e.floor][btn] = 0;
+        if (e.AssignedRequests[e.Floor][btn]){
+            e.AssignedRequests[e.floor][btn] = 0;
             if (onClearedRequest){
                 onClearedRequest(btn, floor)
             }
@@ -353,35 +322,34 @@ func request_clearAtCurrentFloor(e_old Elevator, onClearedRequest(b Button, floo
     return e
 }
 
-func timeToIdle(e Elevator) int{
+func timeToIdle(elev chan config.Elevator) int{
     duration int = 0
 
-    switch e.behaviour {
-    case EB_Idle:
-        e.dirn = requests_chooseDirection(e)
-        if e.dirn == D_Stop {
+    switch elev.State {
+    case ES_IDLE:
+        elev.Direction = requests_chooseDirection(elev)
+        if elev.Direction == elevio.MD_Stop {
             return duration
         }
         break
-    case EB_Moving:
+    case ES_MOVING:
         duration += TRAVEL_TIME/2
-        e.floor += e.dirn
+        elev.Floor += elev.Direction
         break
-    case EB_DoorOpen:
+    case ES_DOOROPEN:
         duration -= DOOR_OPEN_TIME/2
     }
 
     for {
-        if requests_shouldStop(e) {
-            e = request_clearAtCurrentFloor(e, nil)
+        if requests_shouldStop(elev) {
+            elev = request_clearAtCurrentFloor(elev, nil) //????
             duration += DOOR_OPEN_TIME
-            e.dirn = requests_chooseDirection(e)
-            if e.dirn == D_Stop {
+            elev.Direction = requests_chooseDirection(elev)
+            if elev.Direction == elevio.MD_Stop {
                 return duration
             }
         }
-        e.floor += e.direction
+        elev.Floor += elev.Direction
         duration += TRAVEL_TIME
     }
 }
-*/
